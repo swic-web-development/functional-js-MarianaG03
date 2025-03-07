adb79535d3dc15913f5cf5a09ecfd7c7f247d15b
Mariana Garcia <mariana.garcia@student.swic.edu>
2025-03-06 20:33:59 -0600

    Add initial inventory setup and order cost calculation function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-var x = 3;
+const backpackFullOfAmmo = []
 
-console.log(x);
+const cell = { items: 'energy cell', quantity: 20, priceEa: 0.2}
+const clip = { items: 'ammo clip', quantity: 5, priceEa: 0.5}
+const rocket = { items: 'box of rocket', quantity: 5, priceEa: 25.0}
+const shells = { items: 'box of shells', quantity: 8, priceEa: 1.0}
+
+const calcOrderCost = (order) =>  order.quantity * order.priceEa 

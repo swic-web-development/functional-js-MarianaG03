b9973b2c3926bd4192e8d3abee193d2c40c2e5df
Mariana Garcia <mariana.garcia@student.swic.edu>
2025-03-06 21:26:33 -0600

    Add find() method examples to both arrays

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const backpackFullOfAmmo = [
     { name: 'energy cell units', quantity: 20, priceEa: 0.2 },
 ]
 
+
+const alotOfAmmo = backpackFullOfAmmo.find((ammo) => ammo.quantity > 15);
+console.log(alotOfAmmo)
+
 const tableRows = backpackFullOfAmmo.map((ammo) => {
     return `<tr>
         <td>${ammo.name}</td>
@@ -21,10 +25,20 @@ const clip = { items: 'ammo clip', quantity: 5, priceEa: 0.5}
 const rocket = { items: 'box of rocket', quantity: 5, priceEa: 25.0}
 const shells = { items: 'box of shells', quantity: 8, priceEa: 1.0}
 
+
+
 const calcOrderCost = (order) =>  order.quantity * order.priceEa 
  
 const numbers = [1,2,3,4,5]
 
+const found = numbers.find((num, index, array) => {
+    console.log(`Checking ${num} at index ${index} in [${array}]`);
+    return num > 4;
+});
+
+console.log(found);
+
 const doubleNumber = numbers.map((numbers) => numbers * 2)
 
 console.log(doubleNumber)
+
